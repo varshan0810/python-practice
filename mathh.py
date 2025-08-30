228b5961755012bf07b66c27e467a4ce940c64cd
varshansri848@gmail.com <varshansri848@gmail.com>
2025-08-30 21:13:51 +0530

    math.py is done

--- mathh.py
+++ mathh.py
@@ -1,1 +1,7 @@
-print("Code for mathematical operations")
+num_1 = int(input("Enter the first number :"))
+num_2 = int(input("Enter the second number :"))
+
+print(f"Addition : {num_1 +num_2}")
+print(f"Subtraction : {num_1 - num_2}")
+print(f"Multiplication : {num_1 * num_2}")
+print(f"Division : {num_1 // num_2}")
